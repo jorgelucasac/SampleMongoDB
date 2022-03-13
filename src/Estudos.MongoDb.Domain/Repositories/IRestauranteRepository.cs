3de0e1528dc169bab9d8a931fb7b59102ee1e468
jorgelucas <jorgelucasl91@gmail.com>
2022-03-12 21:41:40 -0300

    criação da interface de restaurante repositório

---
+++ src/Estudos.MongoDb.Domain/Repositories/IRestauranteRepository.cs
@@ -0,0 +1,10 @@
+﻿using Estudos.MongoDb.Domain.Entities;
+
+namespace Estudos.MongoDb.Domain.Repositories;
+
+public interface IRestauranteRepository
+{
+    Task<Restaurante> Create(Restaurante restaurante, CancellationToken cancellationToken);
+
+    Task<Restaurante> GetAll(CancellationToken cancellationToken);
+}
